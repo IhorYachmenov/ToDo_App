3f5a2afe86eca50c38b5043d2106b6a9d67a8d87
IhorYachmenov <IlLoganlI@Hotmail.com>
2020-06-04 22:02:37 +0300

    Created copy of fab.

--- app/src/main/java/com/example/todoapp/view/ListOfCategoryActivity.java
+++ app/src/main/java/com/example/todoapp/view/ListOfCategoryActivity.java
@@ -1,7 +1,12 @@
 package com.example.todoapp.view;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -9,30 +14,61 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.todoapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ListOfCategoryActivity extends AppCompatActivity {
 
     CategoryData data;
 
+    FloatingActionButton fab;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_list_category);
+        setContentView(R.layout.activity_list_of_category_data);
 
+        openDialog();
+
+        // Set data from intent
         Intent intent = getIntent();
-
         TextView setLabel = (TextView) findViewById(R.id.label_name);
         ConstraintLayout setColor = (ConstraintLayout) findViewById(R.id.task_list_background);
-
         data = (CategoryData)intent.getSerializableExtra("Class Data");
-
         setColor.setBackgroundColor(data.getBackgroundColor());
         setLabel.setText(data.getLabelName());
 
 
-
-
     }
 
+    public void openDialog() {
+
+        fab = findViewById(R.id.fab_category_activity);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final Dialog dialog = new Dialog(ListOfCategoryActivity.this, android.R.style.Theme_Translucent_NoTitleBar_Fullscreen);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setCancelable(false);
+                dialog.setContentView(R.layout.fragment_floating_button);
+                ColorDrawable dialogColor = new ColorDrawable(Color.WHITE);
+                dialogColor.setAlpha(200);
+                dialog.getWindow().setBackgroundDrawable(dialogColor);
+
+
+                FloatingActionButton dialogButton1 = (FloatingActionButton) dialog.findViewById(R.id.fab_fragment);
+                dialogButton1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+
+            }
+        });
+
+    }
 
 }
